7a51d81c0435ed6a9247dcfddd7f009b46ba392a
bjohnson <b.johnson@escmatrix.com>
2014-05-19 17:26:48 -0400

    hook up js files and grunt

--- src/base/base.js
+++ src/base/base.js
@@ -4,7 +4,31 @@ var Insights = (function(_) {
 		this.s = settings;
 		this.d = data;
 	}
-	
+
+	// return tweet data, sorted by day
+	Insights.prototype.dataByDay = function(){
+		var data = _.cloneDeep(this.d);
+		
+		data = _.sortBy(data,function(tweet){
+			return tweet.created_at;
+		});
+		
+		data = _.groupBy(data,function(tweet){
+			return tweet.created_at;
+		});
+		
+		scrubDataByDay(data);
+		
+		
+		return data;
+	};
+
+	function scrubDataByDay(data){
+		
+		console.log(data);
+		
+	}
 	
 	return Insights;
+
 })(_);
