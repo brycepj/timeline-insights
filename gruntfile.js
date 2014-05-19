5796bb777f1f41963fe2915d191548c6e0de5149
Bryce Johnson <b.johnson@escmatrix.com>
2014-05-19 15:55:09 -0400

    grunt config and file structure

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+	// 1. All configuration goes here
+	grunt
+			.initConfig({
+				pkg : grunt.file.readJSON('package.json'),
+
+				shell : {
+					build : {
+						command : [
+								'git add .',
+								'git commit -m "last commit: <%= grunt.template.today("yyyy-mm-dd") %>"',
+								'git push origin master' ].join('&&')
+					}
+				},
+
+				watch : {
+					scripts : {
+						files : [ 'index.html','src/**/*.js','lib/**/*.js', 'gruntfile.js' ],
+						tasks : [ 'jshint','concat'],
+						options : {
+							livereload : true,
+							spawn : false,
+						},
+					}
+				},
+				
+				concat: {
+					js: {
+						src: ['src/**/*.js'],
+						dest:'dist/timeline-insights.js'
+					},
+					json: {
+						src:['lib/**/*.js'],
+						dest:'dist/lib/libs.js'
+							
+					},
+					dist:{
+						src:['dist/timeline-insights.js','dist/lib/libraries.js'],
+						dest:'dist/timeline-insights.js'
+					}
+				},
+				
+				jshint:{
+					all: ['Gruntfile.js', 'src/**/*.js','lib/**/*.js']
+				}
+						
+			});
+
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-shell');
+
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('dev', [ 'jshint', 'concat','watch' ]);
+	
+
+};
---
+++ src/base/base.js
@@ -0,0 +1,10 @@
+var Insights = (function(_) {
+	
+	function Insights(data, settings) {
+		this.s = settings;
+		this.d = data;
+	}
+	
+	
+	return Insights;
+})(_);
