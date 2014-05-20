a4e4a00ba347e38aacc98ece6436bf743f526b1f
bryce <brycepj@gmail.com>
2014-05-19 23:18:45 -0400

    finish data by date

--- src/base/base.js
+++ src/base/base.js
@@ -1,34 +1,11 @@
-var Insights = (function(_) {
-	
-	function Insights(data, settings) {
-		this.s = settings;
-		this.d = data;
-	}
+var Insights = (function (_) {
 
-	// return tweet data, sorted by day
-	Insights.prototype.dataByDay = function(){
-		var data = _.cloneDeep(this.d);
-		
-		data = _.sortBy(data,function(tweet){
-			return tweet.created_at;
-		});
-		
-		data = _.groupBy(data,function(tweet){
-			return tweet.created_at;
-		});
-		
-		scrubDataByDay(data);
-		
-		
-		return data;
-	};
+    function Insights(data, settings) {
+        this.s = settings;
+        this.d = data;
+    }
 
-	function scrubDataByDay(data){
-		
-		console.log(data);
-		
-	}
-	
-	return Insights;
+    return Insights;
 
 })(_);
+
