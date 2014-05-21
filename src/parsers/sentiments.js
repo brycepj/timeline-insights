1d6adf8901fa8a66f9e5648cbb43061c835ed86f
bjohnson <b.johnson@escmatrix.com>
2014-05-21 11:59:27 -0400

    finish sentiments

--- src/parsers/sentiments.js
+++ src/parsers/sentiments.js
@@ -1,41 +1,72 @@
 Insights.prototype.sentiments = function() {
 
-	var data = this.textForTotals(), fullText = data.fullText, tweets = data.wordLevel, lib = this.sentimentLib();
+	var data = this.textForTotals(), fullText = data.fullText, tweets = data.wordLevel, lib = this
+			.sentimentLib();
 
 	var negativeWords = [], positiveWords = [], balance = 0, negativeTweets = [], positiveTweets = [], neutralTweetCount = 0, totalWords = 0;
 
-
 	for (var i = 0; i < tweets.length; i++) {
-		var tweet = tweets[i];
-		
+		var tweet = tweets[i], currentBalance = 0, currentPositive = [], currentNegative = [];
+
 		for (var j = 0; j < tweet.length; j++) {
 			var word = tweet[j];
-			
-			for (var key in lib) {
+			totalWords++;
+
+			for ( var key in lib) {
 				var sent = key;
 				var score = lib[key];
-				
+
 				if (word === sent) {
 					if (score > 0) {
 						positiveWords.push(sent);
-						
+						currentPositive.push(sent);
+
+						balance += score;
+						currentBalance += score;
+
 					} else {
 						negativeWords.push(sent);
+						currentNegative.push(sent);
+
+						balance += score;
+						currentBalance += score;
 					}
-					
-					balance += score;
+
 				}
 			}
-			
-			
+
 		}
-		
-		
+
+		if (currentBalance > 0) {
+			positiveTweets.push({
+				text : fullText[i],
+				negativeWords : currentNegative,
+				positiveWords : currentPositive,
+				balance : currentBalance
+			});
+		} else if (currentBalance < 0) {
+			negativeTweets.push({
+				text : fullText[i],
+				negativeWords : currentNegative,
+				positiveWords : currentPositive,
+				balance : currentBalance
+			});
+		} else {
+
+			neutralTweetCount++;
+
+		}
+
 	}
-	
-	
-	
-	
+
+	negativeTweets = _.sortBy(negativeTweets, function(tweet) {
+		return tweet.balance;
+	});
+
+	positiveTweets = _.sortBy(positiveTweets, function(tweet) {
+		return tweet.balance;
+	}).reverse();
+
 	return {
 		negativeWords : negativeWords,
 		positiveWords : positiveWords,
